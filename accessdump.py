b245f8b1d4081da808b460da61ca3bb09682b26e
jo <jo@alien>
2014-10-19 19:22:42 +0100

    Tweaked accessdump to have an output directory to keep things tidy

--- accessdump.py
+++ accessdump.py
@@ -2,11 +2,13 @@
 #!/usr/bin/env python
 #
 # AccessDump.py
-# A simple script to dump the contents of a Microsoft Access Database.
+# A simple script to dump the contents of a Microsoft Access Database. Initially
+# copied from http://mazamascience.com/WorkingWithData/?p=168 with some minor changes
+# Thanks Guys!
 # It depends upon the mdbtools suite:
 #   http://sourceforge.net/projects/mdbtools/
 
-import sys, subprocess
+import sys, subprocess, os
 
 DATABASE = sys.argv[1]
 
@@ -15,11 +17,19 @@ table_names = subprocess.Popen(["mdb-tables", "-1", DATABASE],
                                stdout=subprocess.PIPE).communicate()[0]
 tables = table_names.split('\n')
 
-# Dump each table as a CSV file using "mdb-export",
+# Create an output directory below the current one for tidiness
+# Pass if it already exists
+outputdir = os.path.join(os.getcwd(), 'output')
+try:
+	os.makedirs(outputdir, 0755)
+except OSError:
+	pass
+
+# Dump each table as a CSV file uinto output directory using "mdb-export",
 # converting " " in table names to "_" for the CSV filenames.
 for table in tables:
     if table != '':
-        filename = table.replace(" ","_") + ".csv"
+        filename = os.path.join(outputdir, table.replace(" ","_") + ".csv")
         file = open(filename, 'w')
         print("Dumping " + table)
         contents = subprocess.Popen(["mdb-export", DATABASE, table],
